c494f37b24f0a4b3c6fda83dc47b47a995003563
Asifakousar Pathan <Asifakousar.Pathan@godigit.com>
2024-08-02 15:53:26 +0530

    [Asifa] ADD: Adding Remaining Programs

---
+++ PolicyCancellation/CancellationNotAllowedException.java
@@ -0,0 +1,7 @@
+package PolicyCancellation;
+public class CancellationNotAllowedException extends Exception
+{
+    public CancellationNotAllowedException(String message) {
+        super(message);
+    }
+}
