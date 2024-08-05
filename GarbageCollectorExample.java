6eeb9f29b8f4af092df946099529be0cbd1ecc80
Asifakousar Pathan <Asifakousar.Pathan@godigit.com>
2024-08-05 17:52:51 +0530

    Add GarbageColletor

---
+++ GarbageCollectorExample.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class GarbageCollectorExample {
+    public GarbageCollectorExample() {
+        System.out.println("GCExample instance created");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        try {
+            System.out.println("GCExample is being garbage collected");
+        } finally {
+            super.finalize();
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            GarbageCollectorExample example = new GarbageCollectorExample();
+        }
+        System.gc();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
