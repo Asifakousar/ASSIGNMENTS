1cbb258aeeafd91d90b63b2f2eaf568d91f9077e
Asifakousar Pathan <Asifakousar.Pathan@godigit.com>
2024-08-07 11:41:58 +0530

    [Asifa] Add displaycontent

---
+++ displaycontent/Main.java
@@ -0,0 +1,21 @@
+package displaycontent;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("src/displaycontent/input.txt"));
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        } finally {
+            br.close();
+        }
+    }
+}
