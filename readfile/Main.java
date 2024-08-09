af91a2633bd15f15d7f26286feb0973d9385ebbb
Asifakousar Pathan <Asifakousar.Pathan@godigit.com>
2024-08-09 10:56:49 +0530

    readfile

---
+++ readfile/Main.java
@@ -0,0 +1,21 @@
+package readfile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Path filePath = Paths.get("src\\readfile\\text.txt");
+
+        try (Stream<String> lines = Files.lines(filePath)) {
+            lines.forEach(line -> {
+                System.out.println(line);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
